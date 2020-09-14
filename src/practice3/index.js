d2898a7e3a9006af73552d56f403e739635413e3
DoYourLove <xyf15620168540@163.com>
2020-09-14 18:14:51 +0800

    [XiaoYiFan] feat : finish practice3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,5 @@
-export const getPoetry = 1;
+export const getPoetry = async () => {
+  const response = await fetch("https://v1.jinrishici.com/all.json");
+  const data = await response.json();
+  return [data.origin, data.author, data.content];
+};
